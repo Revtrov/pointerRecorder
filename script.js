cc1943c78912487d9f087379176d85b815620b23
Revtrov <73724492+Revtrov@users.noreply.github.com>
2022-11-08 13:11:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ let slope = (p1, p2) => {
     return (p2[1] - p1[1]) / (p2[0] - p1[0])
 }
 let quadDraw = (img, pos) => {
-    ctx.drawImage(img, pos[0], pos[1], 128, 128)
-    ctx.drawImage(img, canvas.width - pos[0], pos[1], 128, 128)
-    ctx.drawImage(img, pos[0], canvas.height - pos[1], 128, 128)
-    ctx.drawImage(img, canvas.width - pos[0], canvas.height - pos[1], 128, 128)
+     //ctx.drawImage(img, pos[0]-64, pos[1]-64, 128, 128)
+     //ctx.drawImage(img, canvas.width - pos[0]-64, pos[1]-64, 128, 128)
+     //ctx.drawImage(img, pos[0]-64, canvas.height - pos[1]-64, 128, 128)
+     //ctx.drawImage(img, canvas.width - pos[0]-64, canvas.height - pos[1]-64, 128, 128)
+
+    
 }
 let splitLine = (splits, p1, p2, img) => {
     let lineSlope = slope(p1, p2);
@@ -27,11 +29,28 @@ let img = new Image();
 img.src = "1200x675mf.jpg"
 
 document.addEventListener("mousemove", (event) => {
-    let currentPoints = [event.clientX - 64, event.clientY - 64]
-        // ctx.beginPath();
-        // ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
-        // ctx.fillRect(0, 0, canvas.width, canvas.height);
-        // ctx.fill()
+    let currentPoints = [event.clientX, event.clientY]
+     ctx.strokeStyle = "blue"
+     ctx.lineWidth = "1"
+     for(let i = 2000; i >-2000; i--){
+     ctx.beginPath();
+     ctx.moveTo(lastPos[0]*Math.sqrt(Math.abs(i), Math.E)-1+1, lastPos[1]*Math.sqrt(Math.abs(i), Math.E)-1+1)
+     ctx.lineTo(currentPoints[0]*Math.sqrt(Math.abs(i), Math.E)-1+1, currentPoints[1]*Math.sqrt(Math.abs(i), Math.E)-1+1)
+     ctx.stroke()   
+     ctx.moveTo(canvas.width-lastPos[0]*Math.sqrt(Math.abs(i), Math.E)-1+1, lastPos[1]*Math.sqrt(Math.abs(i), Math.E)-1+1)
+     ctx.lineTo(canvas.width-currentPoints[0]*Math.sqrt(Math.abs(i), Math.E)-1+1, currentPoints[1]*Math.sqrt(Math.abs(i), Math.E)-1+1)
+     ctx.stroke()
+     ctx.moveTo(lastPos[0]*Math.sqrt(Math.abs(i), Math.E)-1+1, canvas.height - lastPos[1]*Math.sqrt(Math.abs(i), Math.E)-1+1)
+     ctx.lineTo(currentPoints[0]*Math.sqrt(Math.abs(i), Math.E)-1+1, canvas.height -currentPoints[1]*Math.sqrt(Math.abs(i), Math.E)-1+1)
+     ctx.stroke()
+     ctx.moveTo(canvas.width - lastPos[0]*Math.sqrt(Math.abs(i), Math.E)-1+1, canvas.height - lastPos[1]*Math.sqrt(Math.abs(i), Math.E)-1+1)
+     ctx.lineTo(canvas.width-currentPoints[0]*Math.sqrt(Math.abs(i), Math.E)-1+1, canvas.height -currentPoints[1]*Math.sqrt(Math.abs(i), Math.E)-1+1)
+     ctx.stroke()
+        
+    }
+         ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+         ctx.fillRect(0, 0, canvas.width, canvas.height);
+         ctx.fill()
     let dist = Math.sqrt(((lastPos[0] - [currentPoints[0], currentPoints[1]][0]) ** 2) + ((lastPos[1] - [currentPoints[0], currentPoints[1]][1]) ** 2))
     splitLine(dist, lastPos, [currentPoints[0], currentPoints[1]], img)
     lastPos = [currentPoints[0], currentPoints[1]]
